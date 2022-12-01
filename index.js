8cae2dcd6f86ef257f5d8fea38077be8675fe524
Lewis Benson <tm.lewis.benson@gmail.com>
2022-11-30 19:58:17 -0500

    sync index.js to database

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+const { SQLDb } = require('./src/models');
+
+
 const server = require('./src/server');
+
+SQLDb.sync()
+  .then(() => {
+    console.log('Connected');
+  }).catch(err => console.error(err.message));
+  
 server.start();
